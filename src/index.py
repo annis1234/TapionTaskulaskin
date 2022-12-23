a0fcc3c823be3e09e1b7ceb9b4e6ec70a0b8a3ff
Anni Simonen <anni.simonen@helsinki.fi>
2022-12-23 17:59:31 +0200

    korjailua

--- src/index.py
+++ src/index.py
@@ -15,13 +15,13 @@ IMG_PATH = os.path.join(IMG_DIRNAME, "..", "images", "tree.png")
 
 IMG = ImageTk.PhotoImage(Image.open(IMG_PATH))
 HEADER_IMG = ttk.Label(image=IMG)
-HEADER_IMG.place(x=50,y=60)
+HEADER_IMG.place(x=50,y=70)
 HEADER_CRED = ttk.Label(text="Kuva: https://pixabay.com/",
                         font=("Comic Sans MS", 10),
                         foreground="green",
                         cursor="hand1")
 
-HEADER_CRED.place(x=50, y=300)
+HEADER_CRED.place(x=50, y=320)
 HEADER_CRED.bind("<Button-1>", lambda e: webbrowser.open_new_tab("https://pixabay.com/"))
 
 WINDOW.geometry("900x800")
